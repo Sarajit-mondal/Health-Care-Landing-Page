8906495c03cef89b5fd70b0d40c7f15f697e5b5e
sarajit <bdsarajit499@gamil.com>
2024-07-04 11:34:51 +0600

    question section complete

--- src/component/question/Question.jsx
+++ src/component/question/Question.jsx
@@ -7,25 +7,25 @@ function Question() {
        <h2>Frequently Asked Question</h2>
 
        {/* question */}
-       <div className="collapse [&>div]:bg-secondary-color [&>div]:my-1">
+       <div className="collapse bg-secondary-color my-2">
   <input type="radio" name="my-accordion-1" defaultChecked />
-  <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+  <div className="collapse-title text-xl font-medium"> What are your office hours?</div>
   <div className="collapse-content">
-    <p>hello</p>
+    <p>Our office hours vary by location, but typically we are open Monday through Friday from 8:00 AM to 5:00 PM. Some locations may offer extended hours or weekend appointments. Please contact your nearest clinic for specific hours.</p>
   </div>
 </div>
 <div className="collapse bg-secondary-color my-1">
   <input type="radio" name="my-accordion-1" />
-  <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+  <div className="collapse-title text-xl font-medium">How can I schedule an appointment?</div>
   <div className="collapse-content">
-    <p>hello</p>
+    <p>Our office hours vary by location, but typically we are open Monday through Friday from 8:00 AM to 5:00 PM. Some locations may offer extended hours or weekend appointments. Please contact your nearest clinic for specific hours.</p>
   </div>
 </div>
 <div className="collapse bg-secondary-color my-1">
   <input type="radio" name="my-accordion-1" />
-  <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+  <div className="collapse-title text-xl font-medium">Do you offer telemedicine appointments?</div>
   <div className="collapse-content">
-    <p>hello</p>
+    <p>Our office hours vary by location, but typically we are open Monday through Friday from 8:00 AM to 5:00 PM. Some locations may offer extended hours or weekend appointments. Please contact your nearest clinic for specific hours.</p>
   </div>
 </div>
     </div>
